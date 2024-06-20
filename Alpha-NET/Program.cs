7678852fcde4cbd858693ebc19549972afc68a4a
UsoDelduca <usodelduca@gmail.com>
2024-06-21 11:08:46 +1200

    add example classes and config

--- Alpha-NET/Program.cs
+++ Alpha-NET/Program.cs
@@ -1,27 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+
+// Configure DbContext before building the app
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<StudentContext>(options =>
+        options.UseInMemoryDatabase("Student"));
+}
+else
+{
+    builder.Services.AddDbContext<StudentContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("StudentContext") ?? throw new InvalidOperationException("Connection string 'StudentContext' not found.")));
+}
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Enable CORS
+app.UseCors("AllowReactApp");
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
